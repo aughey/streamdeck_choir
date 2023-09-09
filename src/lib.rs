27b3f21954071bb7dd83559664c84a41186b70ea
John Aughey <jha@aughey.com>
2023-09-09 22:51:24 +0000

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,41 @@
 use std::collections::HashMap;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug,Serialize,Deserialize)]
-#[serde(deny_unknown_fields)] 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MyConfig {
+    pub groups: Vec<Group>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Group {
+    pub name: String,
+    pub channels: Vec<(String, u32)>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     version: u32,
     r#type: String,
-    pages: HashMap<String,Page>,
-    instances: HashMap<String,Instance>
+    pub pages: HashMap<String, Page>,
+    pub instances: HashMap<String, Instance>,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            version: 4,
+            r#type: "full".to_string(),
+            pages: (1..100)
+                .into_iter()
+                .map(|i| (i.to_string(), Page::default()))
+                .collect(),
+            instances: Default::default(),
+        }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct Instance {
@@ -19,99 +43,188 @@ pub struct Instance {
     label: String,
     lastUpgradeIndex: u32,
 }
+impl Instance {
+    pub fn new(instance_type: &str, label: &str, upindex: u32) -> Self {
+        Self {
+            instance_type: instance_type.to_string(),
+            label: label.to_string(),
+            lastUpgradeIndex: upindex,
+        }
+    }
+}
 
-#[derive(Debug,Serialize,Deserialize)]
-#[serde(deny_unknown_fields)] 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct Page {
     name: String,
-    controls: HashMap<String,HashMap<String,Control>>,
-    gridSize: Gridsize
+    pub controls: HashMap<String, HashMap<String, Control>>,
+    gridSize: Gridsize,
+}
+impl Default for Page {
+    fn default() -> Self {
+        Self {
+            name: "PAGE".to_string(),
+            controls: Default::default(),
+            gridSize: Default::default(),
+        }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[allow(non_camel_case_types)]
 #[serde(tag = "type")]
-enum Control {
+pub enum Control {
     button(ButtonControl),
-    pageup(PageChange),
-    pagedown(PageChange),
+    pageup,
+    pagedown,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
-#[serde(deny_unknown_fields)] 
-pub struct PageChange {
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// #[serde(deny_unknown_fields)]
+// pub struct PageChange {}
 
-#[derive(Debug,Serialize,Deserialize)]
-#[serde(deny_unknown_fields)] 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct Gridsize {
     minColumn: u32,
     maxColumn: u32,
     minRow: u32,
-    maxRow: u32
+    maxRow: u32,
+}
+impl Default for Gridsize {
+    fn default() -> Self {
+        Self {
+            minColumn: 0,
+            maxColumn: 3,
+            minRow: 0,
+            maxRow: 3,
+        }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct ButtonControl {
     style: Style,
     options: Options,
     feedbacks: Vec<()>,
-    steps: HashMap<String,Step>
+    steps: HashMap<String, Step>,
+}
+impl ButtonControl {
+   pub fn new_page_select(text: impl AsRef<str>, page: u32) -> Self {
+        let mut steps = HashMap::new();
+        steps.insert("0".to_string(), Step::new_page_select(page));
+        Self {
+            style: Style::new(text.as_ref()),
+            options: Default::default(),
+            feedbacks: Default::default(),
+            steps: steps
+        }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct Style {
     text: String,
     size: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     png: Option<()>,
     alignment: String,
     pngalignment: String,
     color: u32,
     bgcolor: u32,
-    show_topbar: String
+    show_topbar: String,
+}
+impl Style {
+    fn new(text: &str) -> Self {
+        Self {
+            text: text.to_string(),
+            size: "auto".to_string(),
+            png: None,
+            alignment: "center:center".to_string(),
+            pngalignment: "center:center".to_string(),
+            color: 16777215,
+            bgcolor: 0,
+            show_topbar: "default".to_string(),
+        }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct Options {
     relativeDelay: bool,
     stepAutoProgress: bool,
-    rotaryActions: Option<bool>
+    #[serde(skip_serializing_if = "Option::is_none")]
+    rotaryActions: Option<bool>,
+}
+impl Default for Options {
+    fn default() -> Self {
+        Self { relativeDelay: false, stepAutoProgress: true, rotaryActions: None }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+fn new_id() -> String {
+    uuid::Uuid::new_v4().to_string()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct Step {
     action_sets: ActionSet,
-    options: StepOptions
+    options: StepOptions,
+}
+impl Step {
+    fn new_page_select(page: u32) -> Self {
+        Self {
+            action_sets: ActionSet {
+                down: vec![Action::set_page_byindex(SetPageByIndexAction {
+                    id: new_id(),
+                    instance: "internal".to_string(),
+                    options: PageOptions {
+                        controller: 0,
+                        page_from_variable: false,
+                        page,
+                        page_variable: page.to_string(),
+                    },
+                    delay: 0,
+                })],
+                up: vec![],
+                rotate_left: None,
+                rotate_right: None,
+            },
+            options: StepOptions { runWhileHeld: vec![] },
+        }
+    }
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct StepOptions {
-    runWhileHeld: Vec<()>
+    runWhileHeld: Vec<()>,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct ActionSet {
     down: Vec<Action>,
     up: Vec<Action>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     rotate_left: Option<Vec<Action>>,
-    rotate_right: Option<Vec<Action>>
+    #[serde(skip_serializing_if = "Option::is_none")]
+    rotate_right: Option<Vec<Action>>,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[allow(non_camel_case_types)]
 #[serde(tag = "action")]
 enum Action {
@@ -120,17 +233,17 @@ enum Action {
     fader_delta(FaderDeltaAction),
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct FaderDeltaAction {
     id: String,
     instance: String,
     options: FadDeltaOptions,
-    delay: u32
+    delay: u32,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct FadDeltaOptions {
@@ -141,17 +254,17 @@ pub struct FadDeltaOptions {
     fadeType: String,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct SetPageByIndexAction {
     id: String,
     instance: String,
     options: PageOptions,
-    delay: u32
+    delay: u32,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct PageOptions {
@@ -161,16 +274,16 @@ pub struct PageOptions {
     page_variable: String,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct FadAction {
     id: String,
     instance: String,
     options: FadOptions,
-    delay: u32
+    delay: u32,
 }
 
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 #[allow(non_snake_case)]
 pub struct FadOptions {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
-use streamdeck_choir::Config;
 
-fn main() {
-    const FILE : &str = "config.json";
-    let filedata = std::fs::read_to_string(FILE).expect("Unable to read file");
+use std::collections::HashMap;
+use anyhow::Result;
+use streamdeck_choir::{Config, Control, Instance, Page, MyConfig, ButtonControl};
 
-    let parsed = serde_json::from_str::<Config>(&filedata).expect("Unable to parse JSON");
+fn main() -> Result<()> {
+    {
+        const FILE: &str = "config.json";
+        let filedata = std::fs::read_to_string(FILE).expect("Unable to read file");
 
-    println!("{:#?}", parsed);
+        let _parsed = serde_json::from_str::<Config>(&filedata).expect("Unable to parse JSON");
+    }
+
+    let myconfig = {
+        const CONFIG_FILE: &str = "config.yml";
+        let filedata = std::fs::read_to_string(CONFIG_FILE).expect("Unable to read file");
+        serde_yaml::from_str::<MyConfig>(&filedata).expect("Unable to parse YAML")
+    };
+
+    let mut config = Config::default();
+
+    for p in 1..100 {
+        let mut page = Page::default();
+        let mut row = HashMap::new();
+
+        for (index,group) in myconfig.groups.iter().enumerate() {
+            let button = ButtonControl::new_page_select(&group.name, (index+1).try_into()?);
+            row.insert(index.to_string(), Control::button(button));
+        }
+
+        // row.insert("0".to_string(), Control::pageup);
+        // row.insert("1".to_string(), Control::pagedown);
+        page.controls.insert("0".to_string(), row);
+
+        config.pages.insert(p.to_string(), page);
+    }
+
+    let x32 = Instance::new("behringer-x32", "x32", 2);
+    let instanceid = uuid::Uuid::new_v4().to_string();
+    config.instances.insert(instanceid, x32);
+
+    // write config to a file in json format
+    let json = serde_json::to_string_pretty(&config).expect("Unable to serialize config");
+    println!("{}", json);
+
+    Ok(())
 }
